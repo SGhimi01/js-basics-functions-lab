516f28d3a0e9345b83caf520aeb16aa1b9b60410
SHISHIR GHIMIRE <sghimi01@gmail.com>
2022-04-01 09:49:30 +1100

    Basic function done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks-42);
+}
+console.log(distanceFromHqInBlocks(50));
+
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance)*264;
+}
+
+function distanceTravelledInFeet(a,b) {
+    return Math.abs(a-b)*264;
+}
+
+function calculatesFarePrice(a,b) {
+    let x = distanceTravelledInFeet(a,b);
+    if (x <=400){ 
+        return 0
+    } else if (x > 400 && x <= 2000) {
+        return (x-400)*0.02
+    } else if (x > 2000 && x < 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
